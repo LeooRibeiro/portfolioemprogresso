66a018d5a0f952e274c693abf708c72e12386698
Leonardo Cassemiro Ribeiro <92592356+LeooRibeiro@users.noreply.github.com>
2024-04-04 15:55:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Codigo para as funções das Tabs
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => tab.addEventListener('click', () => {
+    tabClicked(tab)
+}))
+
+const tabClicked = (tab) => {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active')
+
+    const contents = document.querySelectorAll('.content')
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute("content-id");
+    const content = document.getElementById(contentId);
+    
+    content.classList.add('show')
+}
+
+const currentTabActive = document.querySelector('.tab-btn.active');
+tabClicked(currentTabActive);
